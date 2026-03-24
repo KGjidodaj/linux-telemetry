eddb25ecb46733b019b57e634ad90996c1929f7e
Kristian Gjidodaj <kristiangjidodaj@gmail.com>
2026-03-24 17:17:43 +0000

    feat: Implemented input validation with elif/else and optimized logging using tee

--- system_audit.sh
+++ system_audit.sh
@@ -18,6 +18,8 @@ read -p "Would you like to learn your system metrics?(Yes/No) " User_choice
 
 if [[ "$User_choice" == "yes" || "$User_choice" == "Yes" ]]; then
 
+	#### Logging the metrics into system_audit.log along with showing them to the screen for the user
+	{
 	echo "$DIVIDER"
 	echo "   SYSTEM AUDIT REPORT - $Date_var "
 	echo -e "$DIVIDER\n"
@@ -35,6 +37,7 @@ if [[ "$User_choice" == "yes" || "$User_choice" == "Yes" ]]; then
 	# 2. Disk Check
 	# 'df -h /' shows the space in the root partition (Root)
 	echo "[*] DISK SPACE ALLOCATION (Root /):"
+	### (df -h) to be in human readable form with (/) to output info reovlving the root partition
 	df -h /
 	echo ""
 
@@ -48,14 +51,15 @@ if [[ "$User_choice" == "yes" || "$User_choice" == "Yes" ]]; then
 	echo ""
 	echo "$DIVIDER"
 	echo "       Audit Successfully Completed."
+	} | tee -a "system_audit.log"
 
-	#### Logging the metrics into system_audit.log
-	# Rewriting (metrics>>system_audit.log) so that they are shown to the user as they are saved in the logs!
-	free -h >> system_audit.log
-	df -h >> system_audit.log
-	uptime >> system_audit.log
-	#end of logging into system_audit.log
-else
+elif [[ "$User_choice" == "No" || "$User_choice" == "no" ]]; then
+
 	echo "Goodbye then. Exiting..."
 	sleep 0.5
+
+#if the user does not input yes or no by mistake 
+else
+	echo "Wrong answer format try again"
+
 fi
