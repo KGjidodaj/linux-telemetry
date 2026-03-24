7c8eef1f23d788e397a820b1d483f72152fee2db
Kristian Gjidodaj <kristiangjidodaj@gmail.com>
2026-03-24 21:46:15 +0000

    feat: Initial commit of Python mock data generators(Password/Username)

---
+++ password_generator.py
@@ -0,0 +1,42 @@
+# as r just to help with writing
+import random as r
+
+#creating the function that generates the code
+def gen_password(length):
+    ##specifying pool of characters to choose from
+    pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ##setting the variable equal to a string to avoid future errors
+    password = ""
+
+    for i in range(length): #the loop that appends the random characters to the password
+        random_parameter = r.randint(0,1000)
+        a = r.randint(0,9)
+        b= r.choice(pool)
+        ###creating a randomizer to pick between adding a character or a integer
+        if random_parameter <= 500:
+            password = str(a) + password
+        else:
+            password = b+ password
+
+    return password ## returning it to output the finalized password
+
+while True:
+
+    print("Do you want to create a password (Yes/No)")
+    answer = input()
+
+    if answer == "Yes":
+
+        print("How many characters should your password be?")
+        print("Please add an integer else there will be an error ")
+        length = int(input())
+        print(f"Password: {gen_password(length)}\n")
+
+    elif answer == "No":
+
+        print("Goodbye then...exiting.")
+        break
+
+    else:
+        print("WARNING: Try again answering with the correct format")
+
---
+++ username_generator.py
@@ -0,0 +1,67 @@
+# as r just to help with writing
+import random as r
+
+def username_generator(length):
+    pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    username = "" #reset of the username
+    for i in range(length): #to loop and append a character each time to the string
+        character = r.choice(pool)
+        username = username + character
+    return username
+
+def thank_you(length):
+    ##the pool to choose from with r.choice(pool) below
+    pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345"
+    count = 0 #count to see how many times it took 
+    while True:
+
+        username = "" #reset of the username
+        for i in range(length): #to loop and append a character each time to the string
+            character = r.choice(pool)
+            username = username + character
+        check = username_checker(username)
+        if check == True:
+            print(f"Here is the count for the times it took: {count}")
+            return username #no need for else in a while true
+        if check == False:
+            count = count + 1
+
+def username_checker(username):
+    flag = True
+    ##Fun check of the username for numbers in my version of .isalpha() for numbers
+    for i in range(len(username)):
+        if username[i].isdigit():
+            print(f"Stopping process found number in username: {username[i]}\n Loop restarted ")
+            flag = False #could be anything except True
+            break
+
+    if flag != False:
+        print("Check Done no numbers in string")
+        flag = True #changing the flag to true as no numbers where found
+    return flag
+
+
+#While True so the loop doesnt stop running (except if answer = "No")
+while True:
+
+    print ("Would you like to generate a username?(Yes/No)")
+    answer = input()
+    if answer == "Yes":
+
+        print("How many characters would you like the password to be?")
+        length = int(input()) 
+        print("Would you like to test my version of .isalpha() for numbers?(Yes/No)")
+        answer = input()
+        if answer == "Yes":
+            print(f"Username: {thank_you(length)}\n")
+        else:
+            print(f"Username: {username_generator(length)}\n")
+
+    elif answer == "No":
+
+        print("Goodbye Then...Exiting.")
+        break
+
+    else:
+
+        print("Please input the correct format")
